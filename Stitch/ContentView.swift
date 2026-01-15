77a31fc44080f7ffff1ba912ff6bd03ddb4e6eb4
Thiago Chiquiti <www.chiquiti@gmail.com>
2026-01-15 14:13:15 -0300

    Initial Commit

---
+++ Stitch/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Stitch
+//
+//  Created by Thiago Chiquiti on 15/01/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Stitch/StitchApp.swift
@@ -0,0 +1,17 @@
+//
+//  StitchApp.swift
+//  Stitch
+//
+//  Created by Thiago Chiquiti on 15/01/26.
+//
+
+import SwiftUI
+
+@main
+struct StitchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
